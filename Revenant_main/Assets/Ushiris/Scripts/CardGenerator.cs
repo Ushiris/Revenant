d70b215e921c9bcd99ab092a7f07a532ba927ff3
ushiris_desktop <ushiriscco@gmail.com>
2020-07-07 00:36:42 +0900

    add generator

--- Revenant_main/Assets/Ushiris/Scripts/CardGenerator.cs
+++ Revenant_main/Assets/Ushiris/Scripts/CardGenerator.cs
@@ -14,4 +14,41 @@ public class CardGenerator : MonoBehaviour
 
         return card;
     }
+
+    Card.IDType id_t;
+    uint id;
+
+    private void Start()
+    {
+        id_t = Card.IDType.Normal;
+        id = 0;
+    }
+
+    public GameObject GenerateEnter()
+    {
+        var card = Instantiate(Resources.Load("Card")) as GameObject;
+        var data = card.GetComponent<Card>();
+
+        data.IDtype = id_t;
+        data.Number = id;
+
+        return card;
+    }
+
+    public void SetIDType(Card.IDType type)
+    {
+        id_t = type;
+    }
+
+    public void SetID(string ID)
+    {
+        try
+        {
+            id = uint.Parse(ID);
+        }
+        catch
+        {
+
+        }
+    }
 }
